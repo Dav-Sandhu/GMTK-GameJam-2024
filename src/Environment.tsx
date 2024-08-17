c344aee4fbeb41d9321c2a52d843c3c113da554a
Dav <davanjit@hotmail.com>
2024-08-16 22:37:51 -0400

    retrieve map from json file

--- src/Environment.tsx
+++ src/Environment.tsx
@@ -8,74 +8,26 @@ export default class Environment extends Phaser.Scene{
     preload ()
     {
         this.load.image('tiles', '/tileset.png')
+        this.load.tilemapTiledJSON('map', '/map01.json')
     }
 
     create ()
     {
-        const mapData = new Phaser.Tilemaps.MapData({
-            width: 10,
-            height: 10,
-            tileWidth: 16,
-            tileHeight: 8,
-            orientation: Phaser.Tilemaps.Orientation.ISOMETRIC,
-            format: Phaser.Tilemaps.Formats.ARRAY_2D
-        })
-
-        const map = new Phaser.Tilemaps.Tilemap(this, mapData)
-
+        const map = this.add.tilemap('map')
         const tileset = map.addTilesetImage('tileset', 'tiles')
 
-        const floor = tileset ? map.createBlankLayer('floor', tileset, 400, 300) : null
-        const block = tileset ? map.createBlankLayer('block', tileset, 400, 300) : null
-
-        const data = { 
-            floor: [
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-                [8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
-            ], 
-            block: [
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, 1, 1, 1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, 9, 9, 9, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
-                [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
-            ]
+        if (tileset !== null){
+            map.createLayer('ground', tileset)
+            map.createLayer('game', tileset)
         }
 
-        let y = 0
+        //center the map
+        this.cameras.main.setScroll(
+            (map.widthInPixels - this.cameras.main.width) / 1.7, 
+            (map.heightInPixels - this.cameras.main.height) / 2
+        )
 
-        data.floor.forEach(row => {
-            row.forEach((tile, x) => {
-                floor?.putTileAt(tile, x, y)
-            })
-
-            y++
-        })
-
-        y = 0
-
-        data.block.forEach(row => {
-            row.forEach((tile, x) => {
-                tile !== -1 ? block?.putTileAt(tile, x, y) : null
-            }) 
-
-            y++
-        })
-
-        this.cameras.main.zoomTo(5, 0)
+        this.cameras.main.zoomTo(2, 0)
 
         this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
             if (pointer.isDown) {
